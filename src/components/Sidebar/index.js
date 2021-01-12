3b45ce8b29d5108b7e4bbaeaa6e8215303eb793b
asz33@cornell.edu <asz33@cornell.edu>
2021-01-11 16:24:37 -0800

    Up to 1:09:11

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+    SidebarContainer,
+    Icon,
+    CloseIcon,
+    SidebarWrapper,
+    SidebarMenu,
+    SidebarLink,
+    SideBtnWrap,
+    SidebarRoute,
+} from "./SidebarElements";
+
+const Sidebar = () => {
+    return (
+        <SidebarContainer>
+            <Icon>
+                <CloseIcon />
+            </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    {/* react scroll links! */}
+                    <SidebarLink to="about">About</SidebarLink>
+                    <SidebarLink to="discover">Discover</SidebarLink>
+                    <SidebarLink to="services">Services</SidebarLink>
+                    <SidebarLink to="signup">Sign Up</SidebarLink>
+                </SidebarMenu>
+                <SideBtnWrap>
+                    {/* react router link! */}
+                    <SidebarRoute to="/signin">Sign In</SidebarRoute>
+                </SideBtnWrap>
+            </SidebarWrapper>
+        </SidebarContainer>
+    );
+};
+
+export default Sidebar;
